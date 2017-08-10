42d9ff7a94ec8b095318ea705c68597f9b803d94
Asterrea <theresamendoza1115@yahoo.com>
2017-08-10 21:31:17 +0800

    removing database sqllite from settings.py

--- hope/settings.py
+++ hope/settings.py
@@ -81,13 +81,6 @@ WSGI_APPLICATION = 'hope.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
